bd1a3d676eb254b4e9d117feb792f96e650e1b1c
jinyt <610212587@qq.com>
2023-02-24 13:51:58 +0800

    练习

---
+++ src/exercise/12-插槽.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+class Child extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
+}
+export default class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import App from './exercise/11-非父子-context方案';
+import App from './exercise/12-插槽';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
