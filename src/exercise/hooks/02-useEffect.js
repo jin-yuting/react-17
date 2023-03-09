93df6e9bfc5964dfb59a6d7a537d9330312376e5
jinyt <610212587@qq.com>
2023-03-09 10:32:01 +0800

    练习

--- src/exercise/hooks/02-useEffect.js
+++ src/exercise/hooks/02-useEffect.js
@@ -1,5 +1,4 @@
 import React, { useState,useEffect } from 'react'
-
 export default function App() {
   const [text,setText] = useState('linxi')
   useEffect(()=>{
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 // import App from './App';
 import App from './exercise/hooks/02-useEffect';
 import reportWebVitals from './reportWebVitals';
