bcc3c605503ccec5e1dd907eddec8888dd8aedce
jinyt <610212587@qq.com>
2023-03-09 18:56:19 +0800

    记录

--- src/exercise/hooks/02-useEffect.js
+++ src/exercise/hooks/02-useEffect.js
@@ -1,12 +1,14 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState,useEffect,useRef } from 'react'
 export default function App() {
   const [text,setText] = useState('linxi')
+  const mytext = useRef() // 使用 useRef
   useEffect(()=>{
     setText(text.substring(0).toLocaleUpperCase())
   },[text])//传空数组
   return (
     <div>
       {text}
+      <div ref={mytext}></div>
       <button onClick={()=>{setText('11')}}>add</button>
     </div>
   )
